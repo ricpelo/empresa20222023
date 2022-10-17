1f4df0fee20246bd43c871ed6ce92007f078e8b1
Ricardo Pérez <ricpelo@gmail.com>
2022-10-17 19:30:23 +0200

    Primera versión de la validación en INSERT

--- insertar.php
+++ insertar.php
@@ -13,16 +13,56 @@
     $codigo = obtener_post('codigo');
     $denominacion = obtener_post('denominacion');
 
+    $error = [];
+
     if (isset($codigo, $denominacion)) {
-        $pdo = conectar();
-        $sent = $pdo->prepare("INSERT
+        // Código:
+        $long = mb_strlen($codigo);
+        // La longitud tiene que estar entre 1 y 2:
+        if ($long >= 1 && $long <= 2) {
+            // Todos los caracteres son dígitos:
+            if (ctype_digit($codigo)) {
+                // El código no está repetido:
+                $pdo = conectar();
+                $sent = $pdo->prepare("SELECT COUNT(*)
+                                         FROM departamentos
+                                        WHERE codigo = :codigo");
+                $sent->execute([':codigo' => $codigo]);
+                $cuantos = $sent->fetchColumn();
+                if ($cuantos === 0) {
+                    // El código es válido
+                } else {
+                    $error[] = 'El código ya existe';
+                }
+            } else {
+                $error[] = 'Los caracteres del código no son válidos';
+            }
+        } else {
+            $error[] = 'La longitud del código es incorrecta';
+        }
+
+        // Denominación:
+        $long = mb_strlen($denominacion);
+        // La longitud tiene que estar entre 1 y 255:
+        if ($long >= 1 && $long <= 255) {
+            // La denominación es válida
+        } else {
+            $error[] = 'La longitud de la denominación es incorrecta';
+        }
+
+        if (empty($error)) {
+            $pdo = conectar();
+            $sent = $pdo->prepare("INSERT
                                  INTO departamentos (codigo, denominacion)
                                VALUES (:codigo, :denominacion)");
-        $sent->execute([
-            ':codigo' => $codigo,
-            ':denominacion' => $denominacion,
-        ]);
-        return volver();
+            $sent->execute([
+                ':codigo' => $codigo,
+                ':denominacion' => $denominacion,
+            ]);
+            return volver();
+        } else {
+            // Mostrar los mensajes de error
+        }
     }
     ?>
     <div>
