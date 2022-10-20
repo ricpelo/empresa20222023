bb9d1624a30d211a1e8b00e46a4c59c3a389545b
Ricardo Pérez <ricpelo@gmail.com>
2022-10-20 19:26:32 +0200

    Valida con recursos abstractos

--- auxiliar.php
+++ auxiliar.php
@@ -27,3 +27,82 @@ function obtener_parametro($par, $array)
 {
     return isset($array[$par]) ? trim($array[$par]) : null;
 }
+
+function filtrar_codigo($codigo, &$error)
+{
+    $long = mb_strlen($codigo);
+
+    if ($long < 1 || $long > 2) {
+        insertar_error(
+            'codigo',
+            'La longitud del código es incorrecta',
+            $error
+        );
+    }
+    if (!ctype_digit($codigo)) {
+        insertar_error(
+            'codigo',
+            'Los caracteres del código no son válidos',
+            $error
+        );
+    }
+    if (!isset($error['codigo'])) {
+        $pdo = conectar();
+        $sent = $pdo->prepare("SELECT COUNT(*)
+                                 FROM departamentos
+                                WHERE codigo = :codigo");
+        $sent->execute([':codigo' => $codigo]);
+        $cuantos = $sent->fetchColumn();
+        if ($cuantos !== 0) {
+            insertar_error('codigo', 'El código ya existe', $error);
+        }
+    }
+}
+
+function filtrar_denominacion($denominacion, &$error)
+{
+    $long = mb_strlen($denominacion);
+    if ($long < 1 || $long > 255) {
+        insertar_error(
+            'denominacion',
+            'La longitud de la denominación es incorrecta',
+            $error
+        );
+    }
+}
+
+function insertar_error($campo, $mensaje, &$error)
+{
+    if (!isset($error[$campo])) {
+        $error[$campo] = [];
+    }
+    $error[$campo][] = $mensaje;
+}
+
+function insertar_departamento($codigo, $denominacion)
+{
+    $pdo = conectar();
+    $sent = $pdo->prepare("INSERT
+                             INTO departamentos (codigo, denominacion)
+                           VALUES (:codigo, :denominacion)");
+    $sent->execute([
+        ':codigo' => $codigo,
+        ':denominacion' => $denominacion,
+    ]);
+}
+
+function mostrar_errores($error)
+{
+    foreach ($error as $campo => $mensajes) { ?>
+        <ul>
+            <li>
+                Errores del campo <?= $campo ?><?php
+                foreach ($mensajes as $mensaje) { ?>
+                    <ul>
+                        <li><?= $mensaje ?></li>
+                    </ul><?php
+                } ?>
+            </li>
+        </ul><?php
+    }
+}
--- insertar.php
+++ insertar.php
@@ -16,52 +16,14 @@
     $error = [];
 
     if (isset($codigo, $denominacion)) {
-        // Código:
-        $long = mb_strlen($codigo);
-        // La longitud tiene que estar entre 1 y 2:
-        if ($long >= 1 && $long <= 2) {
-            // Todos los caracteres son dígitos:
-            if (ctype_digit($codigo)) {
-                // El código no está repetido:
-                $pdo = conectar();
-                $sent = $pdo->prepare("SELECT COUNT(*)
-                                         FROM departamentos
-                                        WHERE codigo = :codigo");
-                $sent->execute([':codigo' => $codigo]);
-                $cuantos = $sent->fetchColumn();
-                if ($cuantos === 0) {
-                    // El código es válido
-                } else {
-                    $error[] = 'El código ya existe';
-                }
-            } else {
-                $error[] = 'Los caracteres del código no son válidos';
-            }
-        } else {
-            $error[] = 'La longitud del código es incorrecta';
-        }
+        filtrar_codigo($codigo, $error);
+        filtrar_denominacion($denominacion, $error);
 
-        // Denominación:
-        $long = mb_strlen($denominacion);
-        // La longitud tiene que estar entre 1 y 255:
-        if ($long >= 1 && $long <= 255) {
-            // La denominación es válida
+        if (!empty($error)) {
+            mostrar_errores($error);
         } else {
-            $error[] = 'La longitud de la denominación es incorrecta';
-        }
-
-        if (empty($error)) {
-            $pdo = conectar();
-            $sent = $pdo->prepare("INSERT
-                                 INTO departamentos (codigo, denominacion)
-                               VALUES (:codigo, :denominacion)");
-            $sent->execute([
-                ':codigo' => $codigo,
-                ':denominacion' => $denominacion,
-            ]);
+            insertar_departamento($codigo, $denominacion);
             return volver();
-        } else {
-            // Mostrar los mensajes de error
         }
     }
     ?>
