7e8f0360b6132a3d9a0fd99961a5e4ae0e1e340d
Ricardo Pérez <ricpelo@gmail.com>
2022-10-25 21:01:46 +0200

    Tabla empleados

--- empresa.sql
+++ empresa.sql
@@ -6,6 +6,18 @@ CREATE TABLE departamentos (
     denominacion varchar(255) NOT NULL
 );
 
+DROP TABLE IF EXISTS empleados CASCADE;
+
+CREATE TABLE empleados (
+    id              bigserial     PRIMARY KEY,
+    numero          numeric(4)    NOT NULL UNIQUE,
+    nombre          varchar(255)  NOT NULL,
+    salario         numeric(7, 2) NOT NULL,
+    fecha_nac       timestamp     NOT NULL,
+    departamento_id bigint        NOT NULL
+                                  REFERENCES departamentos (id)
+);
+
 -- Fixtures:
 
 INSERT INTO departamentos (codigo, denominacion)
@@ -14,3 +26,7 @@ INSERT INTO departamentos (codigo, denominacion)
            (30, 'Prevención'),
            (40, 'Laboratorio'),
            (50, 'Automoción');
+
+INSERT INTO empleados (numero, nombre, salario, fecha_nac, departamento_id)
+    VALUES (1000, 'Pepito', 1240, '1982-07-05', 1),
+           (2000, 'María', 2035, '1999-01-01', 4);
